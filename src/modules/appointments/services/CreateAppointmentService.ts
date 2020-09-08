a305d4a967c804c42caff6f147fcc162ada39154
Everton Pereira <evertonsouls@gmail.com>
2020-09-08 23:23:17 +0200

    DDD

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,9 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import AppError from '../errors/AppError';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import Appointiment from '../models/Appointment';
+import AppError from '@shared/errors/AppError';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import Appointiment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 interface Request {
   provider_id: string;
